ebd12aeddbde5ff3623fc1b392b8eaf0cb8064b6
Harshad Shanbhag <harshads@cibsr-mp-302.stanford.edu>
2014-08-13 11:32:21 -0700

    Removed mysql statements

--- mysqlQuery.m
+++
@@ -1,9 +0,0 @@
-function data = mysqlQuery(conn, query_str)
-
-    curs = exec(conn, query_str);
-    results = fetch(curs);
-    
-    data = {};
-    if ~strcmpi(results.Data, 'No Data')
-        data = results.Data;
-    end
